84a2c66e1c6dacd6e8bdfebadd6621afd092f319
Mahesh-J <mahesh162005k@gmail.com>
2026-03-14 08:50:54 +0530

    Implement smooth scrolling for nav links
    Add smooth scrolling for navigation links on portfolio page.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+
 console.log("Portfolio Loaded");
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach(function(link){
+link.addEventListener("click", function(e){
+
+e.preventDefault();
+
+const sectionId = this.getAttribute("href");
+const section = document.querySelector(sectionId);
+
+section.scrollIntoView({
+behavior: "smooth"
+});
+
+});
+});
+
+});
